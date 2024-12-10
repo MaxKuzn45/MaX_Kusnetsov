de37ea705e9338d51ad6e9a903466e46d811e9f6
MaxKuzn45 <maximkuusn.45@gmail.com>
2024-12-10 14:21:49 +0300

    Update lesson_1.py

--- lesson_1.py
+++ lesson_1.py
@@ -1,4 +1,4 @@
-дз 1.1
+#дз 1.1
 import math
 R = float(input("радиус: "))
 L = 2 * math.pi * R
@@ -7,7 +7,7 @@ L_rounded = round(L, 2)
 S_rounded = round(S, 2)
 print(f"L: {L_rounded} см")
 print(f"S: {S_rounded} см²")
-дз 1.2
+#дз 1.2
 x = 10
 y = 55
 print("до:")
@@ -17,7 +17,7 @@ x, y = y, x
 print("после:")
 print(f"x = {x}")
 print(f"y = {y}")
-дз 1.3
+#дз 1.3
 import math
 g = 9.81
 L = float(input("Длина маятника "))
